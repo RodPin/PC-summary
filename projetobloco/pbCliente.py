01658386f30fcf0e9486c56f054181cb03f341a2
rodpin <prodrigo@poli.ufrj.br>
2021-02-02 22:10:39 -0300

    pb cliente servidor

---
+++ projetobloco/pbCliente.py
@@ -0,0 +1,326 @@
+import socket,time,pickle
+import platform
+import psutil
+import pygame, sys
+from pygame.locals import *
+import cpuinfo 
+import os
+import sched 
+import time
+
+
+host =  '172.17.0.2'
+
+
+def request(message):
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        s.connect((host,9998))
+        message = pickle.dumps(message)
+        s.send(message)
+        data = s.recv(4096)
+        s.close()
+        return pickle.loads(data)
+    except Exception as erro:
+        print('Servidor desligado, tentando reconexao')
+        time.sleep(5)
+        return request(message)
+
+scheduler = sched.scheduler(time.time, time.sleep)
+
+BLACK=(0,0,0)
+WHITE=(255,255,255)
+RED=(255,40,0)
+GREEN=(0, 130, 36)
+YELLOW=(238, 238, 0)
+GREY=(60,60,60)
+
+ALTURA_BARRA = 200
+LARGURA_BARRA = 70
+ESPACO_BARRA = 10
+
+ESPACO_BARRAS_E_LEGENDA=10
+TAMANHO_LEGENDA=100
+BORDA=15
+
+clock = pygame.time.Clock()
+
+#  cpus_count + 2 pois a barra de Disco e de memoria sao fixas (2 barras a mais)
+LARGURA_TELA= 1300
+LEGENDA_X=LARGURA_TELA -ESPACO_BARRAS_E_LEGENDA- BORDA - TAMANHO_LEGENDA + ESPACO_BARRAS_E_LEGENDA
+ALTURA_TELA=630
+DISPLAY=pygame.display.set_mode((LARGURA_TELA,ALTURA_TELA),0,32)
+
+
+def inGB(m):
+    if m is not None:
+        return str(round(m/(1024*1024*1024),2))
+
+pygame.init()
+
+def desenhar_barra(label,percentage,i,total=0):
+    #Define a fonte e o tamanho
+    myfont = pygame.font.SysFont("Arial", 15)
+    # Define o tamanho da superficie em que vamos desenhar
+    surface = pygame.surface.Surface((80,273))
+    # Define onde a barra sera desenhada horizontalmente
+    BARRA_X=BORDA +ESPACO_BARRA*i + LARGURA_BARRA*i
+    
+    # Define onde a barra sera desenhada verticalmente
+    BARRA_Y=15
+    
+    
+    # Seta as cores se a porcentagem for menor que 70% verde, entre 70% e 90% amarelo e acima de 90% vermelho
+    COLOR=GREEN
+    if percentage>= 0.9:
+        COLOR=RED
+    elif percentage>=0.7:
+        COLOR=YELLOW
+
+    #Desenha o retangulo branco da barra
+    pygame.draw.rect(surface,WHITE,(0,BARRA_Y,LARGURA_BARRA,ALTURA_BARRA-1))
+    
+    #Desenha o retangulo colorido da barra por cima do branco
+    if(percentage != 0 ):
+        pygame.draw.rect(surface,COLOR,(0,BARRA_Y+ ALTURA_BARRA - percentage*ALTURA_BARRA,LARGURA_BARRA,BARRA_Y+ALTURA_BARRA-(BARRA_Y+ ALTURA_BARRA - percentage*ALTURA_BARRA)))
+        
+
+    #Prepara o texto em baixo da barra
+    label = myfont.render(label, 1, WHITE)
+    porcentagem = myfont.render(str(round(percentage*100)) + '%', 1, WHITE)
+    
+    if total != 0:
+        myfont2 = pygame.font.SysFont("Arial", 11)
+
+        total=float(total)
+        detalhes=myfont2.render(str(int(int(total)*percentage))+'/'+str(int(total))+'GB', 1, WHITE)
+        surface.blit(detalhes,(15 , ALTURA_BARRA + BARRA_Y+30))
+
+    #Desenha na tela o texto
+    surface.blit(porcentagem, (20 , ALTURA_BARRA + BARRA_Y))
+    surface.blit(label, (20 , ALTURA_BARRA + BARRA_Y+15))
+
+    #Desenha a superficie na tela
+    DISPLAY.blit(surface, (BARRA_X, 0))
+
+def desenhar_legenda():
+
+    def desenhar_legenda_unica(cor,label,cont):
+        #Seta a font e tamanho
+        myfont = pygame.font.SysFont("Arial", 12)
+        #Seta a surface
+        surface = pygame.surface.Surface((TAMANHO_LEGENDA+10, 12))
+        #Seta o texto
+        text= myfont.render(label, 1, WHITE)
+        #Desenha o quadrado da cor
+        pygame.draw.rect(surface,cor,(0,0,10,10))
+        #Escreve o texto
+        surface.blit(text, (19 , 0))
+        #Desenha a surface na tela
+        DISPLAY.blit(surface, ( LEGENDA_X, 20+cont*20 ))
+
+    desenhar_legenda_unica(GREEN,'Menor que 70%',0)
+    desenhar_legenda_unica(YELLOW,"Entre 70 e 90%",1)
+    desenhar_legenda_unica(RED,'Maior que 90%',2)
+
+def desenhar_bolinhas(index,array):
+    surface = pygame.surface.Surface((50, 30))
+    circles=len(array)
+    color=RED
+    for i in range(0,circles):
+        if i == index or index==4:
+            color=WHITE
+        else:
+            color=GREY
+        pygame.draw.circle(surface, color, (10+12*i,8),5) 
+    DISPLAY.blit(surface, ( 80,ALTURA_BARRA+70))
+
+def desenhar_info(array):
+    #Seta a font e tamanho
+    myfont = pygame.font.SysFont("Arial", 15)
+    title = pygame.font.SysFont("Arial", 23)
+    surface = pygame.surface.Surface((LARGURA_TELA, 300))
+
+    title_element= title.render(array[0], 1, WHITE)
+    surface.blit(title_element,(40,0))
+
+    surface.blit(myfont.render('Utilize ↑ ↓ para navegar',1, WHITE),(280,0))
+
+    for i,info in enumerate(array):
+        if i!=0:
+            #Seta os textos
+            text= myfont.render(info, 1, WHITE)
+            surface.blit(text, (0 , 30+i*17))
+    
+    DISPLAY.blit(surface,(20,ALTURA_BARRA+100))
+
+def desenhar_monitoramento():
+    resposta = request({'name':'monitoramento'})
+
+
+    desenhar_bolinhas(index,resposta['infos'])
+    desenhar_barra('Memoria',resposta['pct_memoria']/100,0)
+    desenhar_barra('Disco',resposta['pct_disco']/100,1,inGB(resposta['disco'].total))
+    
+    for i,cpu in enumerate(resposta['CPUS']):
+        desenhar_barra('CPU '+str(i+1),cpu/100,2+i)
+        
+
+    desenhar_legenda()
+    desenhar_info(resposta['infos'][index])
+
+def desenha_arquivos():
+    textos=[]
+    resposta = request({'name':'diretorios'})
+    myfont = pygame.font.SysFont("Courier", 20)
+    surface = pygame.surface.Surface((LARGURA_TELA, 900))
+
+    def escrever(label,namePath='',idx=None):
+        textos.append(myfont.render(label,1, WHITE))
+    
+    
+    for linha in resposta['lista']:
+        escrever(linha)
+    escrever(" Total ........  : "+str(resposta['total'])+" Arquivos e Diretorios")
+    escrever(" Total de bytes utilizados : "+str(resposta['bytes_total']/1000)+" Kbytes")
+        
+    for idx,texto in enumerate(textos):
+        surface.blit(texto, (2 , 25*idx))
+
+    DISPLAY.blit(surface,(20,20))
+    end_time=time.process_time()
+    
+
+pagina=0
+max_pagina=0
+
+def desenha_processos():
+    global pagina , max_pagina
+    initial_time=time.process_time()
+    myfont = pygame.font.SysFont("Courier", 16)
+    myfont2 = pygame.font.SysFont("Arial", 16)
+    surface = pygame.surface.Surface((LARGURA_TELA, 2000))
+
+    def escrever(label,idx):
+        txt=myfont.render(label,1, WHITE)
+        surface.blit(txt, (2 ,100 +   20*idx))
+
+    def escrever2(label,idx):
+        surface.blit(myfont2.render(label,1, WHITE), (2 ,100 +   20*idx))
+    resposta = request({'name':'processos','payload':pagina})
+
+    max_pagina = resposta['max']
+    
+    escrever2('Utilize ↑ ↓ para navegar',-4)
+    escrever2('Quantidade de processos: '+str(resposta['qtd']),-3)
+    escrever2('Pagina '+str(pagina+1)+'/'+str(resposta['max']+1),-2)
+
+    for idx,line in enumerate(resposta['pagina']):
+        escrever(line,idx)
+
+       
+    DISPLAY.blit(surface,(20,20))
+    end_time=time.process_time()
+
+
+def desenha_hosts():
+    myfont = pygame.font.SysFont("Courier", 20)
+    surface = pygame.surface.Surface((LARGURA_TELA, 900))
+    surface2 = pygame.surface.Surface((LARGURA_TELA, 200))
+    
+    def escrever(texto):
+        return myfont.render(texto,1, WHITE)
+
+    resposta = request({'name':'hosts'})
+
+    if resposta=='carregando':
+        texto=myfont.render('Consultando informacoes de rede...',1,WHITE)
+        surface.blit(texto, (0 , 0))       
+        DISPLAY.blit(surface,(400,300))
+    else:    
+        for idx,texto in enumerate(resposta):
+            surface.blit(escrever(texto), (2 ,25*idx))       
+         
+    DISPLAY.blit(surface,(20,300))
+
+
+    resposta2 = request({'name':'trafego_rede'})
+
+    for idx,texto in enumerate(resposta2): 
+        surface2.blit(escrever(texto), (2 ,25*idx))             
+
+    DISPLAY.blit(surface2,(20,20))
+
+
+def desenha_navegacao():
+    myfont = pygame.font.SysFont("Arial", 15)
+    surface = pygame.surface.Surface((LARGURA_TELA, 300))
+    surface.blit(myfont.render('Utilize ← ou → para navegar',1, WHITE),(280,0))
+    DISPLAY.blit(surface,(220,ALTURA_TELA-40))
+
+DISPLAY.fill(BLACK)
+index=0
+index_copy=None
+clock.tick(60)
+CPUS=psutil.cpu_percent(interval=0.1, percpu=True)
+
+aba=1
+
+while True:
+    DISPLAY.fill(BLACK)
+
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            sys.exit() 
+        #============================================================== Mudanca de aba
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_LEFT:
+                if aba > 1:
+                    aba=aba-1
+            if event.key == pygame.K_RIGHT:
+                if aba < 4:
+                    aba=aba+1
+        #============================================================== Primeira ABA
+        if aba == 1:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if index > 0  and index != 4:
+                        index=index-1
+                if event.key == pygame.K_DOWN:
+                    if index < 3 and index != 4:
+                        index=index+1
+                if event.key == pygame.K_SPACE:
+                    if index_copy == None:
+                        index_copy=index
+                        index=4
+                    else:
+                        index=index_copy
+                        index_copy=None
+        
+        #============================================================== Segunda Aba
+        if aba==2:
+            print('aba2')
+            #==============================================================
+        if aba ==3:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and pagina < max_pagina:
+                    pagina=pagina+1
+                if event.key == pygame.K_DOWN and pagina > 0:
+                    pagina=pagina-1
+        
+
+    if aba == 1:
+        desenhar_monitoramento()
+    if aba == 2:
+        scheduler.enter(0, 1, desenha_arquivos, ())
+        scheduler.run()
+    if aba == 3:
+        desenha_processos()
+    if aba == 4:
+        desenha_hosts()
+
+    desenha_navegacao()
+    
+    pygame.display.update()
+
---
+++ projetobloco/pbServidor.py
@@ -0,0 +1,351 @@
+import socket
+import psutil
+import platform
+import cpuinfo
+import pickle
+import time
+import os
+from datetime import datetime
+from threading import Thread
+import nmap
+import subprocess
+
+# Cria o socket
+socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+# Obtém o nome da máquina
+host = socket.gethostname()
+
+porta = 9998
+# Associa a porta
+socket_servidor.bind((host, porta))
+# Escutando...
+socket_servidor.listen()
+
+sistema = platform.system()
+
+print("Servidor de nome", host, "esperando conexão na porta", porta)
+
+interface_rede_atual= ['',0]
+redes_infos = psutil.net_io_counters(pernic=True)
+for info_rede in redes_infos:
+    bytes_env=redes_infos[info_rede].bytes_sent
+    if interface_rede_atual[1] < bytes_env:
+        interface_rede_atual[0] =info_rede
+        interface_rede_atual[1]= bytes_env
+
+processador='Processador: '
+array=0 
+sistema = platform.system()
+info = cpuinfo.get_cpu_info()
+processador=''
+if sistema == 'Linux':
+    processador += info['brand_raw']
+elif sistema == 'Windows':
+    array=1
+    processador += platform.processor()
+elif sistema == 'Darwin':
+    processador += info['brand_raw']
+
+#CPU
+cpus_count=psutil.cpu_count()
+arquitetura = 'Arquitetura: '  + info['arch']
+nome= processador
+bits='Palavra: '+str(info['bits']) + ' Bits'
+cpuscount='Núcleos (Logicos): '+str(cpus_count)
+cpuscountfisical='Nucleos (Fisicos): '+str(psutil.cpu_count(logical=False))
+frequencia_total='Frequencia total: '+str(round(psutil.cpu_freq().max,2)) +' MHz'
+
+CPUS=''
+count=0
+def get_monitoramento():
+    global CPUS,count
+    def inGB(m):
+        if m is not None:
+            return str(round(m/(1024*1024*1024),2))
+
+    frequencia='Frequencia: '+ str(round(psutil.cpu_freq().current,2)) + ' MHz'
+
+    #Memoria
+    mem = psutil.virtual_memory()
+    memoria_total="Memoria Total: "+ inGB(mem.total)+ " GB"
+    memoria_disponivel="Memoria Disponivel: "+ inGB(mem.available)+ " GB"
+    memoria_livre="Memoria Livre: "+ inGB(mem.free)+ " GB"
+    memoria_usada="Memoria Usada: "+ inGB(mem.used)+ " GB"
+
+    net= psutil.net_if_addrs()[interface_rede_atual[0]][0]
+    nets=[]
+    ip=''
+    netmask=''
+
+    for i in psutil.net_if_addrs():
+        for x in psutil.net_if_addrs()[i]:
+            if '192.' in x.address:
+                ip= 'IP: '+ str(net.address)
+                if ip not in nets:
+                    netmask='NetMask: '+ str(net.netmask)
+                    family='Family: ' + str(net.family)
+                    ptp='Ptp: '+str(net.ptp)
+                    nets=nets+[ip,netmask,family,ptp]
+    memoria_os=[]
+    net_os=[]
+    if sistema == 'Linux':
+        memoria_buffers="Buffers: "+ inGB(mem.buffers)+ " GB"
+        memoria_cached="Cached: "+ inGB(mem.cached)+ " GB"
+        memoria_shared="Compartilhada: "+  inGB(mem.shared)+ " GB"
+        memoria_slab="Slab: "+ inGB(mem.slab)+ " GB"
+        memoria_ativa="Memoria Ativa: "+ inGB(mem.active)+ " GB"
+        memoria_inativa="Memoria Inativa: "+ inGB(mem.inactive)+ " GB"
+        memoria_os=[memoria_buffers,memoria_cached,memoria_shared,memoria_slab,memoria_ativa,memoria_inativa]
+        net_os=['BroadCast: '+str(net.broadcast)]
+    elif sistema == 'Windows':
+        print("nenhum extra no windows")
+    elif sistema == 'Darwin':
+        memoria_ativa="Memoria Ativa: "+ inGB(mem.active) + " GB"
+        memoria_inativa="Memoria Inativa: "+ inGB(mem.inactive) + " GB"
+        memoria_wired="Wired: "+inGB(mem.wired)+ " GB"
+        memoria_os=[memoria_ativa,memoria_inativa,memoria_wired]
+        net_os=['BroadCast: '+ str(net.broadcast)]
+
+    disco= psutil.disk_usage('.')
+    disco_total='Disco Total: '+ inGB(disco.total) +' GB'
+    disco_usado='Disco Usado: '+ inGB(disco.used)+' GB'
+    disco_livre='Disco Livre: '+ inGB(disco.free)+' GB'
+
+
+
+    processador_info=['Processador:',nome,arquitetura,frequencia_total,frequencia,bits,cpuscount,cpuscountfisical]
+    memoria_info=['Memoria: ', memoria_total,memoria_disponivel,memoria_usada,memoria_livre]+memoria_os
+    disco_info=['Disco: ',disco_total,disco_usado,disco_livre]
+    net_info=['Rede: ']+nets+net_os
+    resumo_info=['Resumo: ',nome,cpuscount,memoria_total,memoria_disponivel,memoria_usada,memoria_livre,disco_total,disco_usado,disco_livre,ip,netmask]
+
+
+    infos=[processador_info,memoria_info,disco_info,net_info,resumo_info]
+
+
+    pct_memoria = psutil.virtual_memory().percent
+    pct_cpu = psutil.cpu_percent()
+    pct_disco = psutil.disk_usage('.').percent
+
+    if count == 0:
+        CPUS=psutil.cpu_percent(percpu=True)
+    count=count+1
+    if count == 4:
+        count=0
+
+    resp = dict()
+    resp['pct_memoria'] = pct_memoria
+    resp['pct_cpu'] = pct_cpu
+    resp['pct_disco'] = pct_disco
+    resp['infos'] = infos
+    resp['CPUS'] = CPUS
+    resp['disco'] = disco
+    
+    return resp
+
+page_size=20
+def get_processos(page):
+    def pegar_info(pid):
+        try:
+            p = psutil.Process(pid)
+            texto = '{:^7}'.format(pid)
+            texto = texto + '{:^11}'.format(p.num_threads())
+            texto = texto + " " + time.ctime(p.create_time()) + " "
+            texto = texto + '{:8.2f}'.format(p.cpu_times().user)
+            texto = texto + '{:8.2f}'.format(p.cpu_times().system)
+            texto = texto + '{:10.2f}'.format(p.memory_percent()) + " MB"
+            rss = p.memory_info().rss/1024/1024
+            texto = texto + '{:10.2f}'.format(rss) + " MB"
+            vms = p.memory_info().vms/1024/1024
+            texto = texto + '{:10.2f}'.format(vms) + " MB"
+            texto = texto + " " + p.exe()
+            return texto
+        except:
+            pass  
+    lista = psutil.pids()
+    aux=0
+    auxidx=0
+    pages=[]
+    for pid in lista:
+        info_process = pegar_info(pid)
+        if info_process != None:
+            if aux==0 and auxidx==0:
+                pages.append([])
+            
+            if auxidx < page_size:
+                pages[aux].append(info_process)
+            else:
+                auxidx=0
+                aux=aux+1
+                pages.append([])
+                pages[aux].append(info_process)
+            auxidx=auxidx+1
+    
+    resp = dict()
+    if page >= len(pages):
+        return {}
+
+    resp['pagina']=pages[page]
+    resp['max']=len(pages)-1
+    resp['qtd']=len(lista)
+
+    return resp
+
+
+def get_diretorios():
+    directs = os.listdir('./')
+    diretorios = dict()
+    totArquivos=0
+    totBytes=0
+    lista = []
+    t="|{:35}|{:>10}|{:21}|{:21}|"
+    lista.append(t.format("Name","Size","Criado em","Modificado em"))
+    
+    for name in directs:
+        namePath = './'+name
+
+        dtatime = datetime.fromtimestamp(os.stat(namePath).st_atime)
+
+        formatdtatime = ("{:{dfmt} {tfmt}}".format(dtatime, dfmt="%d-%m-%Y", tfmt="%H:%M"))
+        dtmtime = datetime.fromtimestamp(os.stat(namePath).st_mtime)
+        formatdtmtime = ("{:{dfmt} {tfmt}}".format(dtmtime, dfmt="%d-%m-%Y", tfmt="%H:%M"))
+
+        text = t.format(name,str(os.stat(namePath).st_size/1000),formatdtatime,formatdtmtime)
+        lista.append(text)
+       
+        totArquivos +=1
+        totBytes = totBytes + os.stat(namePath).st_size
+
+    diretorios['lista'] = lista
+    diretorios['total'] = totArquivos
+    diretorios['bytes_total'] = totBytes
+    return diretorios
+
+def get_trafego_rede():
+    redes_infos = psutil.net_io_counters(pernic=True)
+    t2="|{:^20}|{:^20}|{:^20}|{:^20}|{:^20}|"
+    textos_rede_info=[]        
+    textos_rede_info.append(t2.format('Interface','Bytes enviados','Bytes Recebidos','Pacotes enviados','Pacotes recebidos'))
+    for info_rede in redes_infos:
+        ri_dic=redes_infos[info_rede]
+        label = t2.format(info_rede,ri_dic.bytes_sent,ri_dic.bytes_recv,ri_dic.packets_sent,ri_dic.packets_recv)
+        textos_rede_info.append(label)
+    return textos_rede_info  
+
+def retorna_codigo_ping(hostname):
+    print('hostname')
+    print(hostname)
+    plataforma = platform.system()
+    args = []
+    if plataforma == "Windows":
+        args = ["ping", "-n", "1", "-l", "1", "-w", "100", hostname]
+
+    else:
+        args = ['ping', '-c', '1', '-W', '1', hostname]
+        
+    ret_cod = subprocess.call(args,stdout=open(os.devnull, 'w'),stderr=open(os.devnull, 'w'))
+    return ret_cod
+
+def get_host():
+    global interface_rede_atual
+    aux = psutil.net_if_addrs()[interface_rede_atual[0]][array].address.split('.')
+    aux.pop()
+    return '.'.join(aux)+'.'
+def verifica_hosts(base_ip=get_host()):
+    host_validos = []
+    return_codes = dict()
+    for i in range(1,9):
+        host_editado = base_ip + '{0}'.format(i)
+        return_codes[host_editado] = retorna_codigo_ping(host_editado)
+        if i %20 ==0:
+            print(".", end = "")
+        if return_codes[host_editado] == 0:
+            host_validos.append(host_editado)
+
+    return host_validos
+
+def adiciona_info_portas(nm,host,info_hosts):
+    nm.scan(host,arguments='--exclude-ports 9999')
+    info_hosts[host]={}
+    info_hosts[host]['name']=nm[host]['hostnames'][0]['name']  
+    for proto in nm[host].all_protocols():
+        info_hosts[host]['protocol']=proto
+
+        lport = nm[host][proto].keys()
+        #lport.sort()
+        
+        aux=[]
+        for port in lport:
+            aux.append(str(port))
+        info_hosts[host]['ports']=','.join(aux)
+                
+rede_info= None
+
+def obter_info_hosts():
+    global rede_info
+    hosts_verificados=verifica_hosts()
+    nm = nmap.PortScanner()
+    info_hosts= dict()
+    for host in hosts_verificados:
+        try:
+            adiciona_info_portas(nm,host,info_hosts)
+        except:
+            pass
+        
+    rede_info = info_hosts 
+
+thread_rede = Thread(target = obter_info_hosts, args = ())
+thread_rede.start()
+
+def get_hosts():
+    global interface_rede_atual
+    if thread_rede.is_alive():
+        return 'carregando'
+    else:    
+        textos = []
+        t="|{:^20}|{:^25}|{:^16}|{:^21}|"
+        rede_text='Interface de rede mais utilizada: ' + interface_rede_atual[0]
+        textos.append(rede_text)
+        textos.append(t.format('Host','Nome','Protocolo','Portas'))
+        thread_rede.join()
+        hosts = rede_info
+        UNKNOWN = 'Desconhecido'
+        def getInfo(info):
+            if info in hosts[host]:
+                if hosts[host][info] == '':
+
+                    return UNKNOWN
+                return hosts[host][info]
+            return UNKNOWN
+
+        for idx,host in enumerate(hosts):
+            label = t.format(host,getInfo('name'),getInfo('protocol'),getInfo('ports'))   
+            textos.append(label)
+
+        return textos
+
+while True:
+    (socket_cliente,addr) = socket_servidor.accept()	
+    # Aceita alguma conexão
+    print("Conectado a:", str(addr))
+    resposta = ''
+    msg = socket_cliente.recv(4096)
+    # Decodifica mensagem em ASCII
+    msg = pickle.loads(msg)
+    print(msg)
+    print('Tipo requisicao:',msg['name'])
+    if msg['name'] == 'monitoramento': 
+        resposta = get_monitoramento()
+    if msg['name'] == 'processos':
+        page=msg['payload']
+        resposta = get_processos(page)
+    if msg['name'] == 'diretorios':
+        resposta = get_diretorios()
+    if msg['name'] == 'trafego_rede':
+        resposta = get_trafego_rede()
+    if msg['name'] == 'hosts':
+        resposta = get_hosts()
+    pickled= pickle.dumps(resposta)
+    socket_cliente.send(pickled)
+    socket_cliente.close()
